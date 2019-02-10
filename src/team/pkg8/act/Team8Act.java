16e098b569acb30f261fcf0ab4d1ed5d78656393
reddypramodhini@gmail.com <rahulreddyg16.dr@gmail.com>
2019-02-10 09:16:09 -0500

    changes to orderdetails

--- src/team/pkg8/act/Team8Act.java
+++ src/team/pkg8/act/Team8Act.java
@@ -16,6 +16,15 @@ public class Team8Act {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Customer cust=new Customer();
+        Order order1=new Order();
+        OrderDetail orddetail1=new OrderDetail();
+        Item item1=new Item();
+        Payment payment1= new Payment();
+        Cash cash1=new Cash();
+        check check1=new check();
+        credit credit1=new credit();
+        
     }
     
 }
